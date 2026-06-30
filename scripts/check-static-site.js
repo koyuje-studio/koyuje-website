bedbcdea353e3147681fe673c315b307b2d9e5a6
koyuje-studio <koyuje.studio@gmail.com>
2026-06-30 21:10:42 +0900

    Filter reservation extras by product

--- scripts/check-static-site.js
+++ scripts/check-static-site.js
@@ -268,6 +268,11 @@ const requiredPageSnippets = {
     ".f-input:-webkit-autofill",
     ".extra-in:-webkit-autofill",
     "예약 접수 안내와 촬영 결과물 발송에 사용되니 메일 주소를 한 번 더 확인해 주세요.",
+    'data-extra-for="family"',
+    'data-extra-for="baby"',
+    'data-extra-for="all"',
+    "function updateExtraOptions()",
+    "extra: getVisibleCheckboxes('extra')",
   ],
   "status.html": [
     '<meta name="description" content="고유재 한옥스튜디오 예약 현황 확인. 입금 후 작성한 예약 정보의 접수 확인중, 예약 확인 완료 상태를 휴대폰 번호로 확인하세요.">',
